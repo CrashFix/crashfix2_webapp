e766bb2f5553a142219022abe3d3407dcb0f2413
Mikhail Paulyshka <me@mixaill.tk>
2018-06-28 23:53:58 +0300

    entrypoint: respect X-Real-IP header from reverse proxies

--- index.php
+++ index.php
@@ -13,5 +13,11 @@ if( isset( $_SERVER['CRASHFIX_WEBAPP_DEBUG'] ) )
     defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);    
 }
 
+// respect X-Real-IP header from reverse proxy
+if(isset($_SERVER['HTTP_X_REAL_IP']))
+{
+    $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_X_REAL_IP'];
+} 
+
 require_once($yii);
 Yii::createWebApplication($config)->run();
